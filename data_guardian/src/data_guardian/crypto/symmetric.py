b3f73bf2af90136a5f5744fc3b480f9be131dae1
HPNChanel <phucnguyen20031976@gmail.com>
2025-08-27 00:07:36 +0700

    refactor: main cli, not using Class-Base | refactor: symmetric algo for encrypt and decrypt, remove Class-Base

--- data_guardian/src/data_guardian/crypto/symmetric.py
+++ data_guardian/src/data_guardian/crypto/symmetric.py
@@ -1,25 +1,20 @@
-# Implement the AES-256-GCM encryption engine (OOP).
-from __future__ import annotations
+
 import os
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
-class AesGcm:
-  """AES-256-GCM thin wrapper"""
-  def __init__(self, key: bytes):
-    assert len(key) == 32, "AES-256 requires 32-byte key"
-    self._aes = AESGCM(key)
+AES_KEY_SIZE = 32  #* 256-bit
+NONCE_SIZE = 12
+
+def gen_key() -> bytes:
+  return os.urandom(AES_KEY_SIZE)
   
-  @staticmethod
-  def gen_key() -> bytes:
-    return os.urandom(32)
+def encrypt(key: bytes, plaintext: bytes, aad: bytes = b"")-> tuple[bytes, bytes]:
+  aesgcm = AESGCM(key)
+  nonce = os.urandom(NONCE_SIZE)
+  ct = aesgcm.encrypt(nonce, plaintext, aad)
+  return nonce, ct
   
-  @staticmethod
-  def gen_nonce() -> bytes:
-    return os.urandom(12)
-  
-  def encrypt(self, nonce: bytes, plaintext: bytes, aad: bytes | None = None) -> bytes:
-    return self._aes.encrypt(nonce, plaintext, aad)
-  
-  def decrypt(self, nonce: bytes, ciphertext: bytes, aad: bytes | None = None) -> bytes:
-    return self._aes.decrypt(nonce, ciphertext, aad)
+def decrypt(key: bytes, nonce: bytes, ciphertext: bytes, aad: bytes = b"") -> bytes:
+  aesgcm = AESGCM(key)
+  return aesgcm.decrypt(nonce, ciphertext, aad)
   
