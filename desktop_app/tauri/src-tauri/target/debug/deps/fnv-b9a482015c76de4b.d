8a4e91f70abb4fa0879ffd3f3cf6e8f5a264fca9
HPNChanel <phucnguyen20031976@gmail.com>
2025-10-04 22:25:51 +0700

    fix: cargo build fail

--- desktop_app/tauri/src-tauri/target/debug/deps/fnv-b9a482015c76de4b.d
+++
@@ -1,7 +0,0 @@
-D:\FOR_WORK\PERSONAL_PROJECT\data_guardian\desktop_app\tauri\src-tauri\target\debug\deps\fnv-b9a482015c76de4b.d: C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fnv-1.0.7\lib.rs
-
-D:\FOR_WORK\PERSONAL_PROJECT\data_guardian\desktop_app\tauri\src-tauri\target\debug\deps\libfnv-b9a482015c76de4b.rlib: C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fnv-1.0.7\lib.rs
-
-D:\FOR_WORK\PERSONAL_PROJECT\data_guardian\desktop_app\tauri\src-tauri\target\debug\deps\libfnv-b9a482015c76de4b.rmeta: C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fnv-1.0.7\lib.rs
-
-C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fnv-1.0.7\lib.rs:
--- desktop_app/tauri/src-tauri/target/debug/deps/smallvec-332b225cde3f1936.d
+++
@@ -1,7 +0,0 @@
-D:\FOR_WORK\PERSONAL_PROJECT\data_guardian\desktop_app\tauri\src-tauri\target\debug\deps\smallvec-332b225cde3f1936.d: C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
-
-D:\FOR_WORK\PERSONAL_PROJECT\data_guardian\desktop_app\tauri\src-tauri\target\debug\deps\libsmallvec-332b225cde3f1936.rlib: C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
-
-D:\FOR_WORK\PERSONAL_PROJECT\data_guardian\desktop_app\tauri\src-tauri\target\debug\deps\libsmallvec-332b225cde3f1936.rmeta: C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs
-
-C:\Users\MSI\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.1\src\lib.rs:
